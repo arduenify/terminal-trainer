f39eee95f4460394a1d101e1d9fc987e5ba2711e
Adam S <scoggins.aj@gmail.com>
2023-04-23 19:34:59 -0500

    Revert "User badge controller"

--- backend/middlewares/validationMiddleware.js
+++ backend/middlewares/validationMiddleware.js
@@ -1,9 +1,6 @@
 const { check } = require('express-validator');
 const { validationResult } = require('express-validator');
-const {
-    BadRequestResponse,
-    ValidationErrorResponse,
-} = require('../controllers/responseController');
+const { BadRequestResponse } = require('../controllers/responseController');
 
 // Middleware for validating user registration
 const validateUserRegistration = [
@@ -22,31 +19,21 @@ const validateUserLogin = [
     _handleValidationErrors,
 ];
 
-// Middleware for creating a user badge relationship
-const validateCreateUserBadge = [
-    check('userId').notEmpty().withMessage('User id is required'),
-    check('badgeId').notEmpty().withMessage('Badge id is required'),
-    _handleValidationErrors,
-];
-
 // If there are any validation errors, send a response with the errors,
 // otherwise, move on to the next middlware.
 async function _handleValidationErrors(req, res, next) {
     const errors = validationResult(req);
 
-    if (errors.isEmpty()) return next();
-
-    return new ValidationErrorResponse(
-        {
+    if (!errors.isEmpty()) {
+        return new BadRequestResponse({
             errors: errors.array(),
-        },
-        res,
-    );
+        });
+    } else {
+        next();
+    }
 }
 
 module.exports = {
     validateUserRegistration,
     validateUserLogin,
-    validateCreateBadge,
-    validateCreateUserBadge,
 };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userRoutes = require('./userRoutes');
-const badgeRoutes = require('./badgeRoutes');
-const userBadgeRoutes = require('./userBadgeRoutes');
 
 router.use('/users', userRoutes);
-router.use('/user-badges', userBadgeRoutes);
 
 module.exports = router;
