7c0a7013a8f9ade1c300f1dc48c1f6df8c7b6def
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-23 19:29:28 -0500

    Added User Badge controller Added a middleware to check for admin status updated the validation middleware handling function and added a new function for validating fields for creating a user badge Added the routes for user badge

---
+++ backend/controllers/userBadgeController.js
@@ -0,0 +1,57 @@
+const { UserBadge, Badge } = require('../models');
+const { CreatedResponse } = require('./responseController');
+
+const getUserBadges = async (req, res) => {
+    const { userId } = req.params;
+
+    if (userId !== req?.user?.id) {
+        return new UnauthorizedResponse(null, res);
+    }
+
+    try {
+        const userBadges = await UserBadge.findAll({
+            where: {
+                userId,
+            },
+            include: {
+                model: Badge,
+                as: 'badge',
+            },
+        });
+
+        return new SuccessResponse(userBadges, res);
+    } catch (err) {
+        const errorMessage =
+            err.message ||
+            'Internal server error when attempting to fetch user badges.';
+
+        return new InternalServerErrorResponse({ error: errorMessage }, res);
+    }
+};
+
+const assignBadgeToUser = async (req, res) => {
+    const { badgeId, userId } = req.body;
+
+    try {
+        const badge = await Badge.findByPk(badgeId);
+
+        if (!badge) {
+            return new NotFoundResponse(null, res);
+        }
+
+        const userBadge = await UserBadge.create({ badgeId, userId });
+
+        return new CreatedResponse(userBadge, res);
+    } catch (err) {
+        const errorMessage =
+            err.message ||
+            'Internal server error when attempting to assign badge to user.';
+
+        return new InternalServerErrorResponse({ error: errorMessage }, res);
+    }
+};
+
+module.exports = {
+    getUserBadges,
+    assignBadgeToUser,
+};
