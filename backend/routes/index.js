8dd71a5ef3fdae6b83ffae3372ed82172c97c0f6
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-23 19:32:41 -0500

    "Appended badge router to primary router"

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userRoutes = require('./userRoutes');
+const badgeRoutes = require('./badgeRoutes');
+const userBadgeRoutes = require('./userBadgeRoutes');
 
 router.use('/users', userRoutes);
+router.use('/badges', badgeRoutes);
 
 module.exports = router;
