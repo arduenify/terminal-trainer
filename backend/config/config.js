5d05f8c3a47e73d25ba8543ff00fbefed70ed27e
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-25 03:13:57 -0500

    Updated database config file

--- backend/config/config.js
+++ backend/config/config.js
@@ -12,9 +12,12 @@ module.exports = {
     production: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: 'postgres',
         logging: false,
+        dialectOptions: {
+            searchPath: process.env.DB_SCHEMA,
+        },
     },
 };
