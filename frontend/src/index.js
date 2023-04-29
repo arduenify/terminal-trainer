493bd48a1dcb942449ac4d936af392106d2b01b8
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-29 15:26:59 -0500

    Cleaned up codes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/store';
+
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
