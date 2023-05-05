9d5415f2558ccae327aecf172407bc3d93ee7b99
Adam Scoggins <scoggins.aj@gmail.com>
2023-05-04 20:21:34 -0500

    Updated the app to use css variables

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -8,13 +8,14 @@ import ModernLoader from '../features/modernLoader';
 import NotificationContext from '../features/notification/context/NotificationContext';
 import Notification from '../features/notification';
 import LoginForm from '../features/login';
+import ExercisesPage from '../features/exercise/page';
 import './App.css';
 
 function App() {
     const { loading } = useLoader();
     const { notification, dismissNotification } =
         useContext(NotificationContext);
-    
+
     return (
         <>
             <Header />
@@ -22,6 +23,7 @@ function App() {
                 <Route path='/' element={<HomePage />} />
                 <Route path='/signup' element={<SignupForm />} />
                 <Route path='/login' element={<LoginForm />} />
+                <Route path='/exercises' element={<ExercisesPage />} />
             </Routes>
             <ModernLoader show={loading} />
             {notification && (
