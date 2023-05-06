7159632ed5af71365a77a83f22f9c3d76435a7a5
Adam Scoggins <scoggins.aj@gmail.com>
2023-05-06 16:13:13 -0500

    Updated the terminal to fix a state snapshot issue. Fixed packages

--- frontend/src/features/exercise/terminal/Terminal.js
+++ frontend/src/features/exercise/terminal/Terminal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useMemo, useState } from 'react';
+import React, { useEffect, useRef, useMemo } from 'react';
 import { Terminal as Xterm } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 import { WebLinksAddon } from 'xterm-addon-web-links';
@@ -9,97 +9,75 @@ import './Terminal.css';
 const Terminal = ({ onCommand }) => {
     const terminalRef = useRef(null);
     const xtermRef = useRef(null);
+    const inputBuffer = useRef('');
+    const commandHistory = useRef([]);
 
-    // Initialize xterm addons
     const fitAddon = useMemo(() => new FitAddon(), []);
     const webLinksAddon = useMemo(() => new WebLinksAddon(), []);
-
-    // Tracks the currently inputted command line
-    const [inputBuffer, setInputBuffer] = useState('');
-
-    // Tracks the input history (todo)
-    const [history, setHistory] = useState([]);
-
-    // Set custom terminal theme
     const customTheme = useMemo(
         () => ({
             background: '#0e0e0e',
             foreground: 'green',
             cursor: '#ffffff',
             selection: 'rgba(255, 255, 255, 0.3)',
+            textSize: '25px',
         }),
         [],
     );
 
-    // Wrapper function for writing to the terminal with optional new line
     const writeToTerminal = (str, newLine = false) => {
-        if (newLine) {
-            xtermRef.current.writeln(str);
-        } else {
-            xtermRef.current.write(str);
-        }
+        newLine ? xtermRef.current.writeln(str) : xtermRef.current.write(str);
     };
 
     const updatePrompt = () => {
-        const currentDirectory = '/home/demo'; // todo
+        const currentDirectory = '/home/demo';
         xtermRef.current.write(`\x1B[32m${currentDirectory}\x1B[m $ `);
     };
 
     // Initialize the terminal on component mount
     useEffect(() => {
         xtermRef.current = new Xterm({ cursorBlink: true, theme: customTheme });
-
         xtermRef.current.loadAddon(fitAddon);
         xtermRef.current.loadAddon(webLinksAddon);
         xtermRef.current.open(terminalRef.current);
 
         writeToTerminal('\x1B[31mWelcome to Terminal Trainer!\x1B[m\r\n');
         updatePrompt();
-        setInputBuffer('');
 
-        // Handles reading input from the terminal
         xtermRef.current.onKey(({ key, domEvent }) => {
             if (key === '\r') {
-                // Enter key pressed
-                const output = onCommand(inputBuffer);
-
-                // Output result on new line
+                // Enter key
+                const output = onCommand(inputBuffer.current);
                 writeToTerminal('', true);
                 writeToTerminal(output, true);
-
-                // Reset the input buffer
-                setInputBuffer('');
-
-                // Add to history
-                setHistory((prev) => [...prev, inputBuffer]);
-
-                // Display the prompt again
+                commandHistory.current = [
+                    ...commandHistory.current,
+                    inputBuffer.current,
+                ];
+                inputBuffer.current = '';
                 updatePrompt();
             } else if (key === '\x7F') {
-                // Backspace key pressed
-                if (inputBuffer.length > 0) {
-                    setInputBuffer((prev) => prev.slice(0, -1));
+                // Backspace key
+                if (inputBuffer.current.length > 0) {
+                    inputBuffer.current = inputBuffer.current.slice(0, -1);
                     writeToTerminal('\b \b');
                 }
             } else {
-                // Handle other keys
                 const keyCode = domEvent.keyCode;
                 const isArrowKey = keyCode >= 37 && keyCode <= 40;
 
                 if (!isArrowKey) {
-                    setInputBuffer((prev) => prev + key);
+                    inputBuffer.current += key;
                     writeToTerminal(key);
                 }
             }
         });
 
-        // Clean up on unmount
         return () => {
             xtermRef.current.dispose();
         };
     }, []);
 
-    // Handle terminal resizing
     useEffect(() => {
         const resizeObserver = new ResizeObserver(() => {
             requestAnimationFrame(() => {
@@ -109,7 +87,6 @@ const Terminal = ({ onCommand }) => {
 
         resizeObserver.observe(terminalRef.current);
 
-        // Clean up on unmount
         return () => {
             resizeObserver.disconnect();
         };
